6ef6df83cae62f615947921ddabfb10af8a40dd0
Abram Young <young@physics.arizona.edu>
2024-10-23 14:13:17 -0700

    check and update history

--- level0.8/src/set_bad_chflag.py
+++ level0.8/src/set_bad_chflag.py
@@ -9,48 +9,62 @@ from pybaselines import Baseline, utils
 from astropy import constants as const
 
 def doStuff(scan):
+    # open fits file
     hdu    = fits.open(scan, mode='update')
+    # read header
+    header = hdu[0].header
+    npix   = header['NPIX']
+    line   = header['LINE']
+    # read data_table
     data   = hdu[1].data
     spec   = data['spec'] 
-    CHANNEL_FLAG = data['CHANNEL_FLAG']
-    npix   = hdu[0].header['NPIX']
-    line   = hdu[0].header['LINE']
     nrow   = len(spec)
-    spec   = data['spec']
+    CHANNEL_FLAG = data['CHANNEL_FLAG']
+
+    if 'HISTORY' not in header:
+        print("No history entries found in the header .. continuing")
+    else:
+        history_list = header['HISTORY']
+        if f'known bad {line} channels flagged' in history_list:
+            print('flagging already done .. stopping')
+            exit
 
     # Known spikes
     # LO Interference
-    # MHz   Channel
-    # 317-342   65, 66, 67, 68
-    # 649-659   133, 134
-    # 972       198, 199
+    # MHz       Channel
+    # 317-342   31-36   66-70
+    # 649-659   66-69   134-136
+    # 972       100-101 201-203
     #
     # Iridium
-    # 1610      329, 330
+    # 1610      165-168 331-335
     # Look through all of the spectra to set row_flags for noisy data
     if(line=='NII'):
         for i in range(nrow-1):
-            #for j in range(npix-1):
-            #    CHANNEL_FLAG[i][j]   = 0
             CHANNEL_FLAG[i][31:36]   = 1    # LO 1 330 MHz
             CHANNEL_FLAG[i][66:69]   = 1    # LO 2 656 MHz
             CHANNEL_FLAG[i][100:101] = 1    # LO 3 980 MHz
             CHANNEL_FLAG[i][165:168] = 1    # Iridium 1 1616-1625 MHz
+
+        hdu[0].header.add_history('known bad NII channels flagged')
+
     elif(line=='CII'):
         for i in range(nrow-1):
-            #for j in range(npix-1):
-            #    CHANNEL_FLAG[i][j]   = 0
             CHANNEL_FLAG[i][66:70]   = 1    # LO 1 330 MHz
             CHANNEL_FLAG[i][134:136] = 1    # LO 2 656 MHz
             CHANNEL_FLAG[i][201:203] = 1    # LO 3 980 MHz
             CHANNEL_FLAG[i][331:335] = 1    # Iridium 1 1616-1625 MHz
 
+        hdu[0].header.add_history('known bad CII channels flagged')
+
+    else:
+        hdu[0].header.add_history('error, no NII or CII line found')
+
     # Write the change back to the fits file
-    print(scan)
     hdu.flush()
 
 
-directory = "/home/young/Projects/GUSTO/level0.8/ACS5"
+directory = "/home/young/Projects/GUSTO/level0.8/ACS3"
 partial = sys.argv[1]
 search_files = sorted(glob.glob(f"{directory}/{partial}.fits"))
 for file in (search_files):
