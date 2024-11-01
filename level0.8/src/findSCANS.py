10d4c3c827dbdeedd71581af7e011ece76042250
Abram Young <young@physics.arizona.edu>
2024-11-01 09:27:24 -0700

    clean some debugs

--- level0.8/src/findSCANS.py
+++ level0.8/src/findSCANS.py
@@ -7,8 +7,6 @@ import time
 import numpy as np
 import datetime
 from influxdb import InfluxDBClient
-#from influxdb_client import InfluxDBClient
-from tqdm import tqdm
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
@@ -34,14 +32,13 @@ bucket = f'{database}/{retention_policy}'
 client = InfluxDBClient('localhost', 8086, '', '', 'gustoDBlp')
 
 # half-beam size for searching l,b grid
-size = 0.5*u.degree
+size = 0.1*u.degree
 
 # image coordinate center
 gc = SkyCoord(l=351.0*u.degree, b=0.0*u.degree, frame='galactic')
-c = gc.transform_to(FK5)
 # image size 
 l_img = 6.0*u.degree
-b_img = 2.0*u.degree
+b_img = 4.0*u.degree
 
 
 start_l  = gc.l.deg - l_img.value/2
@@ -53,37 +50,42 @@ start_b  = gc.b.deg - b_img.value/2
 end_b    = gc.b.deg + b_img.value/2
 N_b      = int(b_img.value/(2*size.value))
 b_indx   = np.linspace(start_b, end_b, N_b)
-print(N_l)
-print(N_b)
-print(l_indx)
-print(b_indx)
 
 my_list=[]
 
-for l in tqdm(l_indx, desc='Main Loop', leave=True):
+for l in l_indx:
     numi += 1
     numj = 0
-    for b in tqdm(b_indx, desc=f"loop for l = {l}", leave=False):
+    for b in b_indx:
         numj += 1
         # find all points in udpPointing where we pointed at ra, dec
         gci = SkyCoord(l=l*u.degree, b=b*u.degree, frame='galactic')
         c = gc.transform_to(FK5)
         myquery = f'SELECT * FROM "udpPointing" WHERE RA<{(c.ra.value +size.to(u.deg).value)}  AND \
                                                       RA>{(c.ra.value -size.to(u.deg).value)}  AND \
-                                                     DEC<{(c.dec.value+size.to(u.deg).value)}  AND \
-                                                     DEC>{(c.dec.value-size.to(u.deg).value)}' 
+                                                      DEC<{(c.dec.value+size.to(u.deg).value)} AND \
+                                                      DEC>{(c.dec.value-size.to(u.deg).value)}' 
         points = client.query(myquery).get_points()
+        print(points)
 
         # For loop over all of these pointings
         # POINTS contains a (time, scanID) for each pointing at (ra,dec)
+        current_list = []
         for point in points:
-            my_list.append(point.get("scanID"))
+            current_list.append(point.get("scanID"))
+
+        my_list.append(current_list)
+
+        if current_list:
+            print(l, b, 1)
+        elif not current_list:
+            print(l, b, 0)
 
 
-prefix = "ACS5_"
-suffix = "_6_L09.fits"
+prefix = "ACS3_"
+suffix = "_5_L09.fits"
 scanID_list = sorted(list(set(my_list)))
 filenames = [f"{prefix}{num}{suffix}" for num in scanID_list]
-print("\n".join(filenames))
+#print("\n".join(filenames))
 
 
