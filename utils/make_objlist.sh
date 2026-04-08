03fb103a289ed890d992bcae927dca63af7f44a1
Russell Shipman <russ@sron.nl>
2026-04-08 10:39:06 +0200

    create seq/scan list

--- utils/make_objlist.sh
+++ utils/make_objlist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-for i in level0.9/NII*fits; do 
+# Create a list of objects an sequence / scan numbers for either CII or NII data
+# the output of this script should be redirected to a file.list
+for i in level0.9/CII*fits; do 
 	echo -n $i; ftlist $i[0] K include="OBJECT"; 
 	done
 
