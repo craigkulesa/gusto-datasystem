1e66bffb51d71f8248a5279a25e51cd7e4b088b4
Russell Shipman <russ@sron.nl>
2026-09-24 10:52:09 +0200

    remove bad data points from a cube

--- utils/cleancube.py
+++ utils/cleancube.py
@@ -2,6 +2,7 @@ import os
 import sys
 import glob
 import numpy as np
+import numpy.ma as ma
 from astropy.io import fits
 from astropy import units
 from astropy.coordinates import SkyCoord, Galactic
@@ -29,19 +30,33 @@ hdr0  = hdu[0].header
 weight = hdu[1].data
 hdrw   = hdu[1].header
 
-cube = np.zeros_like(cube0)
-w = np.zeros_like(weight)
+#cube = np.zeros_like(cube0)
+#w = np.zeros_like(weight)
 
-qgood = np.where((weight > 5.0) & (weight < 1000) & ( cube0 > -4) & (cube0 < 10) & np.isfinite(cube0) & np.isfinite(weight))
-cube[qgood] = cube0[qgood].copy()
-w[qgood] = weight[qgood].copy()
+qgood = np.where((weight > 5.0) & (weight < 1000) & ( cube0 > -10) & (cube0 < 20) & np.isfinite(cube0) & np.isfinite(weight))
+cube = ma.masked_invalid(cube0)
+w = ma.masked_invalid(weight)
+ma.masked_greater(cube,20,False)
+ma.masked_less(cube,-10,False)
+cube = ma.masked_where((w < 3.0),cube,True) 
+cube = ma.masked_where((w > 1000),cube,True) 
+print(cube.shape)
+vels=cube.shape[0]
+lats=cube.shape[1]
+lons=cube.shape[2]
 
+#clean the data along legs at each velocity (simple median subtract)
+for v in np.arange(vels):
+    velslice = cube[v,:,:]
+    bgd = ma.median(velslice,axis=0)
+    cube[v,:,:] -= bgd
 
-medw = np.median(w[qgood])
+#outcube = np.zeros_like(cube0)
 
+medw = ma.median(w)
 print(cube.min(),cube.max(),w.max(),w.min(),medw)
 
-hdulist = fits.PrimaryHDU(cube,hdr0)
+hdulist = fits.PrimaryHDU(ma.filled(cube,-9999),hdr0)
 outfile = f'{target}_{line}_clean.fits'
 hdulist.writeto(outfile,overwrite=True)
 
